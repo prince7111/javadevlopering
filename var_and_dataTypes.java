010ae963003cbbc5200f29c69312ded76e2c0f9b
prince7111 <princemeena7111@gmail.com>
2025-07-05 13:44:08 +0530

    prog. with java CH.1

---
+++ var_and_dataTypes.java
@@ -0,0 +1,8 @@
+package javadevlopering;
+
+public class var_and_dataTypes {
+    public static void main(String args[]){
+        System.out.println("Chapter 1St: variables and datatypes");
+    }
+    
+}
