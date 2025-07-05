ecbf9e5b37d60d997c4ec41590494328a134baac
prince7111 <princemeena7111@gmail.com>
2025-07-05 14:17:48 +0530

    Sum of two number using Scanner

--- var_and_dataTypes.java
+++ var_and_dataTypes.java
@@ -1,15 +1,19 @@
 package javadevlopering;
+import java.util.*;
+
 
 public class var_and_dataTypes {
     public static void main(String args[]){
-        System.out.println("Chapter 1St: variables and datatypes");
-        //sum of a and b
-        System.out.println("simple java program Sum of two number");
-        int a =10;
-        int b = 5;
+      
+        //sum of a and b Using Scanner
+       Scanner prince = new Scanner (System.in);
+       System.out.println("Enter your number firs:");
+        int a = prince.nextInt(); // input frome user
+         System.out.println("Enter your number second:");
+        int b = prince.nextInt(); // input from user
         int sum = a+b;
-        System.out.println(sum);
-        
+        System.out.println("the sum of "+a+" + "+b+" is ="+sum);
+
     }
     
 }
