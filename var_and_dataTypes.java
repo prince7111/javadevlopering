f3ac97c1d45d9981d164319256f2203c0851448f
prince7111 <princemeena7111@gmail.com>
2025-07-05 13:51:04 +0530

    Sum of a and b

--- var_and_dataTypes.java
+++ var_and_dataTypes.java
@@ -3,6 +3,13 @@ package javadevlopering;
 public class var_and_dataTypes {
     public static void main(String args[]){
         System.out.println("Chapter 1St: variables and datatypes");
+        //sum of a and b
+        System.out.println("simple java program Sum of two number");
+        int a =10;
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+        
     }
     
 }
