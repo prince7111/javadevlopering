f13ac681c293453b13611545239cc9229c91bfe2
prince7111 <princemeena7111@gmail.com>
2025-07-06 12:09:11 +0530

    Q.1 avrage of three

--- var_and_dataTypes.java
+++ var_and_dataTypes.java
@@ -5,14 +5,19 @@ import java.util.*;
 public class var_and_dataTypes {
     public static void main(String args[]){
       
-        //sum of a and b Using Scanner
-       Scanner prince = new Scanner (System.in);
-       System.out.println("Enter your number firs:");
-        int a = prince.nextInt(); // input frome user
-         System.out.println("Enter your number second:");
-        int b = prince.nextInt(); // input from user
-        int sum = a+b;
-        System.out.println("the sum of "+a+" + "+b+" is ="+sum);
+        Scanner sc = new Scanner(System.in);
+        //Q.1 avrage of three number
+        System.out.println("Entere your number First:");
+        int n1 = sc.nextInt();
+         System.out.println("Entere your number second:");
+        int n2 = sc.nextInt();
+         System.out.println("Entere your number Third:");
+        int n3 = sc.nextInt();
+        int avrage = (n1+n2+n3)/3;
+        System.out.println("the avrage :");
+        System.out.println(avrage);
+    
+
 
     }
     
