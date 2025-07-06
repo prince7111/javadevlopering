8e1db79f7e43a644aecb4db3c27e591755a4d12c
prince7111 <princemeena7111@gmail.com>
2025-07-06 12:19:38 +0530

    Q.2 Area of Suare (adding);

--- var_and_dataTypes.java
+++ var_and_dataTypes.java
@@ -6,16 +6,21 @@ public class var_and_dataTypes {
     public static void main(String args[]){
       
         Scanner sc = new Scanner(System.in);
-        //Q.1 avrage of three number
-        System.out.println("Entere your number First:");
-        int n1 = sc.nextInt();
+       // Q.1 avrage of three number
+         System.out.println("Entere your number First:");
+         int n1 = sc.nextInt();
          System.out.println("Entere your number second:");
-        int n2 = sc.nextInt();
-         System.out.println("Entere your number Third:");
-        int n3 = sc.nextInt();
-        int avrage = (n1+n2+n3)/3;
-        System.out.println("the avrage :");
-        System.out.println(avrage);
+         int n2 = sc.nextInt();
+          System.out.println("Entere your number Third:");
+         int n3 = sc.nextInt();
+         int avrage = (n1+n2+n3)/3;
+         System.out.println("the avrage :");
+         System.out.println(avrage);//  problem starting frome basics its start and i become hard devloper
+         //Q.2 Area of Square
+         int side = sc.nextInt();
+         int AreaofSuare = side*side; //area of suare fromula : side*side;
+         System.out.println("the area of square is="+AreaofSuare);
+
     
 
 
