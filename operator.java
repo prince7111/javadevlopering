31f293c1d253a3022756736fce8746ff0289348d
prince7111 <princemeena7111@gmail.com>
2025-07-07 21:06:57 +0530

    Q.1 chapter 2 compleate successfullly

---
+++ operator.java
@@ -0,0 +1,14 @@
+package javadevlopering;
+
+public class operator {
+    public static void main(String args[]){
+        System.out.println("all is well");
+        //Q.1 what will be the answer of the following expressions :-
+        int x =2,y = 5;
+        int exp1 = (x * y / x);
+        int exp2 = (x*(y / x));
+        System.out.println(exp1 +" ");
+        System.out.println(exp2 +" ");
+    }
+    
+}
