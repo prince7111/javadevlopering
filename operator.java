ab070b233f2d004dd76e79fbc9ca79847ce34c0d
prince7111 <princemeena7111@gmail.com>
2025-07-07 21:14:26 +0530

    Complete Chaper 2 Operators basics of java(07 07 2025 july months)

--- operator.java
+++ operator.java
@@ -9,6 +9,16 @@ public class operator {
         int exp2 = (x*(y / x));
         System.out.println(exp1 +" ");
         System.out.println(exp2 +" ");
+        //q.2  (Same Questions)
+        int x2 = 200, y2 =50, z2 =100;
+        if(x2 > y2 && y2 > z2){
+            System.out.println("hello");
+
+        }if(z2 > y2 && z2 < x2){
+            System.out.println("java");
+        }if((y2+200) < x2 && (y2+150) < z2){
+            System.out.println("hello world");
+        }
     }
     
 }
