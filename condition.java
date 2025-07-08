8b2c52468dd7a934f4615f5ea5b096349e00fb28
prince7111 <princemeena7111@gmail.com>
2025-07-09 00:22:56 +0530

    Q.1 Compleate successfully

--- condition.java
+++ condition.java
@@ -4,6 +4,13 @@ public class condition {
     public static void main(String args[]){
         System.out.println("hello world III"); // Chapter 03 =Condions //08-07-2025 july
         System.out.println("JAVA // CHAPTER 03 : CONDITION \\"); //description or 
+        //Q.1 check fever tempreture simple problem (temp given)
+        double tempreture = 103.5;
+        if(tempreture > 100){
+            System.out.println("Youe Have To Fever");
+        }else{
+            System.out.println("You Not Have Fever");
+        }
 
     }
     
