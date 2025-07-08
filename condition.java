902ca80e293e5618832dfe7abb3be88cdf8e9410
prince7111 <princemeena7111@gmail.com>
2025-07-09 00:01:01 +0530

    Basics Java-Ch-03-Conditions

---
+++ condition.java
@@ -0,0 +1,10 @@
+package javadevlopering;
+
+public class condition {
+    public static void main(String args[]){
+        System.out.println("hello world III"); // Chapter 03 =Condions //08-07-2025 july
+        System.out.println("JAVA // CHAPTER 03 : CONDITION \\"); //description or 
+
+    }
+    
+}
