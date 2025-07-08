dcde369547fc8ed92334004cf04956f1f82583e7
prince7111 <princemeena7111@gmail.com>
2025-07-09 01:29:49 +0530

    Q.3 Compleate Switch case//conditions

--- condition.java
+++ condition.java
@@ -2,23 +2,45 @@ package javadevlopering;
 
 public class condition {
     public static void main(String args[]){
-        System.out.println("hello world III"); // Chapter 03 =Condions //08-07-2025 july
-        System.out.println("JAVA // CHAPTER 03 : CONDITION \\"); //description or 
-        //Q.1 check fever tempreture simple problem (temp given)
-        double tempreture = 103.5;
-        if(tempreture > 100){
-            System.out.println("Youe Have To Fever");
-        }else{
-            System.out.println("You Not Have Fever");
-        }
-        //Q.2 positive or negetive
-        int number =0;
-        if(number>0){
-            System.out.println("it is positive number");
-        }else if(number < 0){
-            System.out.println("negetive numbers");
-        }else{
-            System.out.println("its zero or not defined number-");
+        // System.out.println("hello world III"); // Chapter 03 =Condions //08-07-2025 july
+        // System.out.println("JAVA // CHAPTER 03 : CONDITION \\"); //description or 
+        // //Q.1 check fever tempreture simple problem (temp given)
+        // double tempreture = 103.5;
+        // if(tempreture > 100){
+        //     System.out.println("Youe Have To Fever");
+        // }else{
+        //     System.out.println("You Not Have Fever");
+        // }
+        // //Q.2 positive or negetive
+        // int number =0;
+        // if(number>0){
+        //     System.out.println("it is positive number");
+        // }else if(number < 0){
+        //     System.out.println("negetive numbers");
+        // }else{
+        //     System.out.println("its zero or not defined number-");
+        // }
+        //Q.3 Switch case day in week
+        int day =5;
+        switch (day) {
+            case 1:System.out.println("Sunday");
+                
+                break;
+              case 2:System.out.println("monday");
+                
+                break;
+                  case 3:System.out.println("tuesday");
+                
+                break;
+                  case 4:System.out.println("wednesday");
+                
+                break;
+                  case 5:System.out.println("thursday");
+                
+                break;
+                case 6: System.out.println("friday");
+            default:System.out.println("Saturday");
+                break;
         }
 
     }
