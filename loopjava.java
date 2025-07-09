8a904abf0967d7ce0ea731337a3273b45cae41f1
prince7111 <princemeena7111@gmail.com>
2025-07-09 21:27:42 +0530

    java basics chapter 04 -loops start (july 9th)

---
+++ loopjava.java
@@ -0,0 +1,10 @@
+package javadevlopering;
+public class loopjava{
+    public static void main(String args[]){
+        System.out.println("Hello World (IV)");
+        System.out.println("Java || Ch-04-||Loops");
+        
+
+
+    }
+}
