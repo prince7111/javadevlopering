fed89581eba9866c2760b08611147533bdc43b7d
prince7111 <princemeena7111@gmail.com>
2025-07-09 22:15:37 +0530

    Q.1 comleat succesfully

--- loopjava.java
+++ loopjava.java
@@ -3,8 +3,12 @@ public class loopjava{
     public static void main(String args[]){
         System.out.println("Hello World (IV)");
         System.out.println("Java || Ch-04-||Loops");
-        
+        //Q.1:How many times hello is printed
+        for(int i=0; i<5; i++){
+            System.out.println("hello");//two times
 
+            i+=2;
+        }
 
     }
 }
