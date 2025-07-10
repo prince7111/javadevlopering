0945139db5605f0a1e6a84b5f49549b248dd72b2
prince7111 <princemeena7111@gmail.com>
2025-07-10 14:19:11 +0530

    Java Ch-05-Paterns

---
+++ paternjava.java
@@ -0,0 +1,10 @@
+package javadevlopering;
+public class paternjava {
+    public static void main(String args[]){
+        // pattern part I /Ch-05
+        System.out.println("Hello World (V)");
+        System.out.println("||JAVA programing || Chapter 05 || Pattern Basics||");
+
+    }
+    
+}
