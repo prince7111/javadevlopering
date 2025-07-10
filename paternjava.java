44c7509361f67979ca8e62e6c80a181c262404e9
prince7111 <princemeena7111@gmail.com>
2025-07-10 14:49:14 +0530

    Q.2: inver. rot. HALF PYRAMID Pattern

--- paternjava.java
+++ paternjava.java
@@ -5,17 +5,28 @@ public class paternjava {
         System.out.println("Hello World (V)");
         System.out.println("||JAVA programing || Chapter 05 || Pattern Basics||");
         //Q,1 hollow ractangle pattern ***
-        int n =4;
-        for(int i =1; i<=n; i++){
-            for(int j =1; j<=n ; j++){
-                if(i==1 || j==1 || i==n || j==n){
-                System.out.print("* ");
-                }else{
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
+        // int n =4;
+        // for(int i =1; i<=n; i++){
+        //     for(int j =1; j<=n ; j++){
+        //         if(i==1 || j==1 || i==n || j==n){
+        //         System.out.print("* ");
+        //         }else{
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
 
+        // }
+        //Q.2 INVERTED AND ROATATE HALF PYRAMID PATTERN
+        int n =4;
+        for(int i=1; i<=n; i++){
+            for(int space =n-i; space>=1; space--){ //spaces print 3,2,1,0time
+                System.out.print("  ");
+           }
+           for(int j =1;j<=i;j++){                  //coloumn me star parinting
+            System.out.print("* ");
+           }
+           System.out.println();//next Line
         }
       
 
