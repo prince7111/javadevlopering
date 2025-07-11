97557e34d3f6e6b5c464a781b0a8f5a5e63ac810
prince7111 <princemeena7111@gmail.com>
2025-07-11 23:36:04 +0530

    Java Functions 11th july missing today but tommarow we will be continue after feee paper,,

---
+++ functions.java
@@ -0,0 +1,8 @@
+package javadevlopering;
+public class functions {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD VI)");
+        System.out.println("|| Java Ch.06 || FUNCTIONS IN JAVA ||");
+    }
+    
+}
