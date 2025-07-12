e89710f71828567c5f3a61af21df7a4d48b3efcd
prince7111 <princemeena7111@gmail.com>
2025-07-12 20:38:58 +0530

    Q.1 Avrage of three number (method/function)

--- functions.java
+++ functions.java
@@ -1,8 +1,13 @@
 package javadevlopering;
 public class functions {
+    public static float clcAvg(int a,int b,int c){
+        return (a+b+c)/3.0f;
+    }
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD VI)");
-        System.out.println("|| Java Ch.06 || FUNCTIONS IN JAVA ||");
+     
+      //  Q.1 write a java programe to compute the average of three number
+      System.out.println(clcAvg(3, 3, 3));
+
     }
     
 }
