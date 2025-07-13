249af36993c26a18f782ef2203f592e3ebc0f535
prince7111 <princemeena7111@gmail.com>
2025-07-13 18:55:14 +0530

    Q.4 Diomond Pattern

--- funTwo.java
+++ funTwo.java
@@ -14,8 +14,32 @@ public class funTwo {
             System.out.println();
         }
     }
+    // Q.4 Diomond pattern 
+    public static void DiomondPat(int n){
+        //firs half
+        for(int i =1; i<=n; i++){
+            for( int j =n-i; j>=1; j--){
+                System.out.print(" ");
+            }
+            for(int k =1; k<= (2*i)-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // second half
+        for(int i =n; i>=1; i--){
+                        for( int j =n-i; j>=1; j--){
+                System.out.print(" ");
+            }
+            for(int k =1; k<= (2*i)-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
     public static void main(String args[]){
-        numPyramid(5);
+       DiomondPat(5);
   
      
         
